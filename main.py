25ac5a10bac04704517752b01d21c1cdae5d344c
Helge <h.herthum@gmail.com>
2024-07-25 16:50:53 +0200

    - set up write-seq scripts to be used as functions

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from packages.simulate_seq import simulate_seq
 from packages.plot_sim import plot_sim
 
 seq_name = "tse_pypulseq"
+write_sequence = False
+simulate_sequence = True
+plot_simulation = True
 
-write_seq(plot=False, write_seq=True, seq_filename=seq_name + '.seq')
-
-simulate_seq(save=True, seq_filename=seq_name + '.seq')
-
-plot_sim(plot={
-    "phantom": True,
-    "seq": True,
-    "kspace": True,
-    "reco": True
-    }, seq_filename=seq_name) 
+if write_sequence:
+    write_seq(plot=False, write_seq=True, seq_filename=seq_name + '.seq')
+    
+if simulate_sequence:
+    simulate_seq(save=True, seq_filename=seq_name + '.seq')
+    
+if plot_simulation:
+    plot_sim(plot={
+        "phantom": True,
+        "seq": True,
+        "kspace": True,
+        "reco": True
+        }, seq_filename=seq_name) 
