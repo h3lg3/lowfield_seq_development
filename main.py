6bcaa5d150160346064b836ae2a16d31a77dd69a
Helge <h.herthum@gmail.com>
2024-08-16 13:44:55 +0200

    - settings for lumina sequence

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ seq_name = seq_name + '_lumina'
 # FOV
 # ======
 fov = (220e-3, 220e-3, 220e-3)
-nk = (32, 32, 1)
+nk = (120, 120, 1)
 
 write_sequence = True
 analyze_sequence = False
-simulate_sequence = True
-plot_simulation = True
+simulate_sequence = False
+plot_simulation = False
 
 if write_sequence:
     if plot_simulation:
@@ -53,3 +53,28 @@ if plot_simulation:
         "kspace": False,
         "reco": True
         }, seq_filename=seq_name) 
+    
+
+# # Lumina test setting
+# fov = (220e-3, 220e-3, 220e-3)
+# nk = (120, 120, 1)
+
+#     seq = tse_3d.constructor(
+#                             echo_time=16e-3,
+#                             repetition_time=2000e-3,   # 600
+#                             etl=10, # define max sampling period (tmax = 200ms?), etl_max = round(tmax/esp), nr. of pe1 steps should be multiple of etl
+#                             dummies=5,     # 5
+#                             ro_bandwidth=20e3,
+#                             ro_oversampling = 2, 
+#                             rf_duration = 100e-6,
+#                             fov=Dimensions(x=fov[0], y=fov[1], z=fov[2]),  
+#                             n_enc=Dimensions(x=nk[0], y=nk[1], z=nk[2]),           
+#                             trajectory=Trajectory.SYMMETRIC,
+#                             refocussing_angle=120/180 * pi,    # pi
+#                             excitation_phase=pi/2,
+#                             refocussing_phase=0,
+#                             channel_ro="x", 
+#                             channel_pe1="y",
+#                             channel_pe2="z",
+#                             system=system
+#                             )[0]
--- write_tse_3d_ptb.py
+++ write_tse_3d_ptb.py
@@ -14,15 +14,15 @@ def main(plot:bool, write_seq:bool, seq_filename:str = "tse_3d_ptb.seq",
     seq = tse_3d.constructor(
                             echo_time=16e-3,
                             repetition_time=2000e-3,   # 600
-                            etl=8, # define max sampling period (tmax = 200ms?), etl_max = round(tmax/esp), nr. of pe1 steps should be multiple of etl
+                            etl=10, # define max sampling period (tmax = 200ms?), etl_max = round(tmax/esp), nr. of pe1 steps should be multiple of etl
                             dummies=5,     # 5
                             ro_bandwidth=20e3,
                             ro_oversampling = 2, 
                             rf_duration = 100e-6,
                             fov=Dimensions(x=fov[0], y=fov[1], z=fov[2]),  
                             n_enc=Dimensions(x=nk[0], y=nk[1], z=nk[2]),           
-                            trajectory=Trajectory.LINEAR,
-                            refocussing_angle=180/180 * pi,    # pi
+                            trajectory=Trajectory.SYMMETRIC,
+                            refocussing_angle=120/180 * pi,    # pi
                             excitation_phase=pi/2,
                             refocussing_phase=0,
                             channel_ro="x", 
