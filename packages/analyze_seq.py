- develop t1 mapping - reported bug with arbitrary gradients

packages/analyze_seq.py
@@ -10,90 +10,90 @@ def analyze_seq(seq_filename: str, system:pp.Opts, seq_path: str = "./sequences/
     seq = pp.Sequence(system=system)
     seq.read(seq_path + seq_filename + ".seq", detect_rf_use = True)
 # %% Analyze sequence
-    # Plot sequence
-    # if seq.definitions['k_space_encoding1']*seq.definitions['k_space_encoding2'] > 900:
-    #     print("Analyzing only first 20% of sequence")
-    #     seq.plot(time_range = (0, round(0.2*seq.duration()[0])))
-    # else:
-    #     seq.plot()
+    #Plot sequence
+    if seq.definitions['k_space_encoding1']*seq.definitions['k_space_encoding2'] > 900 or seq.duration()[0] > 600:
+        print("Analyzing only first 20% of sequence")
+        seq.plot(time_range = (0, round(0.2*seq.duration()[0])))
+    else:
+        seq.plot()
     
-    # # Plot k-space trajectory
-    # k_traj_adc, k_traj = seq.calculate_kspace()[0:2]
+    # Plot k-space trajectory
+    k_traj_adc, k_traj = seq.calculate_kspace()[0:2]
 
+    plt.figure()
+    plt.title('full k-space trajectory ($k_{x}$ x $k_{y}$)')
+    plt.plot(k_traj[0],k_traj[1])
+    plt.plot(k_traj_adc[0],k_traj_adc[1],'.')
+    plt.xlabel(r'k_x [1/m]')
+    plt.ylabel(r'k_y [1/m]')
+    plt.show()
+    
+    # # %% Analyze waveforms: Gradient amplitude and slew rates:: 
+    # # Combined amplitudes and slew rate for case of oblique scanning interesting
+    # # Values should stay below system specs even for oblique scanning
+    wf = {}
+    if seq.definitions['k_space_encoding1']*seq.definitions['k_space_encoding2'] > 900:
+        wf_grad = seq.waveforms(time_range = (0, round(0.2*seq.duration()[0])))
+    else:
+        wf_grad = seq.waveforms()
+
+    wf['t_gx'] = wf_grad[0][0]
+    wf['t_gy'] = wf_grad[1][0]
+    wf['t_gz'] = wf_grad[2][0]
+
+    wf['gx'] = wf_grad[0][1]
+    wf['gy'] = wf_grad[1][1]
+    wf['gz'] = wf_grad[2][1]
+    min_t = np.min(np.concatenate((wf['t_gx'], wf['t_gy'], wf['t_gz'])))
+    max_t = np.max(np.concatenate((wf['t_gx'], wf['t_gy'], wf['t_gz'])))
+    dt = 10e-6
+    wf_interp = {}
+    wf_interp['t'] = np.arange(min_t, max_t, dt)
+    # Gradient amplitude, unit mT/m
+    wf_interp['gx'] = np.interp(wf_interp['t'], wf['t_gx'], wf['gx'])/(seq.system.gamma*1e-3)
+    wf_interp['gy'] = np.interp(wf_interp['t'], wf['t_gy'], wf['gy'])/(seq.system.gamma*1e-3)
+    wf_interp['gz'] = np.interp(wf_interp['t'], wf['t_gz'], wf['gz'])/(seq.system.gamma*1e-3)
+    wf_interp['g_norm'] = np.sqrt(pow(wf_interp['gx'], 2.0) + pow(wf_interp['gy'], 2.0) + pow(wf_interp['gz'], 2.0))
+    # Slew rate, unit T/m/s
+    wf_interp['slew_x'] = np.diff(wf_interp['gx'])/dt*1e-3
+    wf_interp['slew_y'] = np.diff(wf_interp['gy'])/dt*1e-3
+    wf_interp['slew_z'] = np.diff(wf_interp['gz'])/dt*1e-3
+    wf_interp['slew_norm'] = np.sqrt(pow(wf_interp['slew_x'], 2.0) + pow(wf_interp['slew_y'], 2.0) + pow(wf_interp['slew_z'], 2.0))
+    # # Gradient defined by vertices
     # plt.figure()
-    # plt.title('full k-space trajectory ($k_{x}$ x $k_{y}$)')
-    # plt.plot(k_traj[0],k_traj[1])
-    # plt.plot(k_traj_adc[0],k_traj_adc[1],'.')
-    # plt.xlabel(r'k_x [1/m]')
-    # plt.ylabel(r'k_y [1/m]')
+    # plt.title('gradients')
+    # plt.plot(wf['t_gx'],wf['gx'], '.-', c='r')
+    # plt.plot(wf['t_gy'],wf['gy'], '.-', c='g')
+    # plt.plot(wf['t_gz'],wf['gz'], '.-', c='b')
+    # plt.xlabel("time in s")
+    # plt.ylabel("gradient amplitude in Hz/m")
+    # plt.legend(["x", "y", "z"])
     # plt.show()
-    
-    # # # %% Analyze waveforms: Gradient amplitude and slew rates:: 
-    # # # Combined amplitudes and slew rate for case of oblique scanning interesting
-    # # # Values should stay below system specs even for oblique scanning
-    # wf = {}
-    # if seq.definitions['k_space_encoding1']*seq.definitions['k_space_encoding2'] > 900:
-    #     wf_grad = seq.waveforms(time_range = (0, round(0.2*seq.duration()[0])))
-    # else:
-    #     wf_grad = seq.waveforms()
+    # Gradient amplitudes
+    fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
-    # wf['t_gx'] = wf_grad[0][0]
-    # wf['t_gy'] = wf_grad[1][0]
-    # wf['t_gz'] = wf_grad[2][0]
+    # Gradient amplitudes
+    axs[0].plot(wf_interp['t'], wf_interp['gx'], c='r')
+    axs[0].plot(wf_interp['t'], wf_interp['gy'], c='g')
+    axs[0].plot(wf_interp['t'], wf_interp['gz'], c='b')
+    axs[0].plot(wf_interp['t'], wf_interp['g_norm'], c='k')
+    axs[0].set_xlabel("time [s]")
+    axs[0].set_ylabel("gradient amplitude [mT/m]")
+    axs[0].legend(["x", "y", "z", "norm"], loc='upper right')
+    axs[0].set_title('Gradient Amplitudes')
 
-    # wf['gx'] = wf_grad[0][1]
-    # wf['gy'] = wf_grad[1][1]
-    # wf['gz'] = wf_grad[2][1]
-    # min_t = np.min(np.concatenate((wf['t_gx'], wf['t_gy'], wf['t_gz'])))
-    # max_t = np.max(np.concatenate((wf['t_gx'], wf['t_gy'], wf['t_gz'])))
-    # dt = 10e-6
-    # wf_interp = {}
-    # wf_interp['t'] = np.arange(min_t, max_t, dt)
-    # # Gradient amplitude, unit mT/m
-    # wf_interp['gx'] = np.interp(wf_interp['t'], wf['t_gx'], wf['gx'])/(seq.system.gamma*1e-3)
-    # wf_interp['gy'] = np.interp(wf_interp['t'], wf['t_gy'], wf['gy'])/(seq.system.gamma*1e-3)
-    # wf_interp['gz'] = np.interp(wf_interp['t'], wf['t_gz'], wf['gz'])/(seq.system.gamma*1e-3)
-    # wf_interp['g_norm'] = np.sqrt(pow(wf_interp['gx'], 2.0) + pow(wf_interp['gy'], 2.0) + pow(wf_interp['gz'], 2.0))
-    # # Slew rate, unit T/m/s
-    # wf_interp['slew_x'] = np.diff(wf_interp['gx'])/dt*1e-3
-    # wf_interp['slew_y'] = np.diff(wf_interp['gy'])/dt*1e-3
-    # wf_interp['slew_z'] = np.diff(wf_interp['gz'])/dt*1e-3
-    # wf_interp['slew_norm'] = np.sqrt(pow(wf_interp['slew_x'], 2.0) + pow(wf_interp['slew_y'], 2.0) + pow(wf_interp['slew_z'], 2.0))
-    # # # Gradient defined by vertices
-    # # plt.figure()
-    # # plt.title('gradients')
-    # # plt.plot(wf['t_gx'],wf['gx'], '.-', c='r')
-    # # plt.plot(wf['t_gy'],wf['gy'], '.-', c='g')
-    # # plt.plot(wf['t_gz'],wf['gz'], '.-', c='b')
-    # # plt.xlabel("time in s")
-    # # plt.ylabel("gradient amplitude in Hz/m")
-    # # plt.legend(["x", "y", "z"])
-    # # plt.show()
-    # # Gradient amplitudes
-    # fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+    # Slew rates
+    axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_x'], c='r')
+    axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_y'], c='g')
+    axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_z'], c='b')
+    axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_norm'], c='k')
+    axs[1].set_xlabel("time [s]")
+    axs[1].set_ylabel("slew rate [T/m/s]")
+    axs[1].legend(["x", "y", "z", "norm"], loc='upper right')
+    axs[1].set_title('Slew Rates')
 
-    # # Gradient amplitudes
-    # axs[0].plot(wf_interp['t'], wf_interp['gx'], c='r')
-    # axs[0].plot(wf_interp['t'], wf_interp['gy'], c='g')
-    # axs[0].plot(wf_interp['t'], wf_interp['gz'], c='b')
-    # axs[0].plot(wf_interp['t'], wf_interp['g_norm'], c='k')
-    # axs[0].set_xlabel("time [s]")
-    # axs[0].set_ylabel("gradient amplitude [mT/m]")
-    # axs[0].legend(["x", "y", "z", "norm"], loc='upper right')
-    # axs[0].set_title('Gradient Amplitudes')
-
-    # # Slew rates
-    # axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_x'], c='r')
-    # axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_y'], c='g')
-    # axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_z'], c='b')
-    # axs[1].plot(wf_interp['t'][0:-1], wf_interp['slew_norm'], c='k')
-    # axs[1].set_xlabel("time [s]")
-    # axs[1].set_ylabel("slew rate [T/m/s]")
-    # axs[1].legend(["x", "y", "z", "norm"], loc='upper right')
-    # axs[1].set_title('Slew Rates')
-
-    # plt.tight_layout()
-    # plt.show()
+    plt.tight_layout()
+    plt.show()
     # %% Sequence test report
     # For the real TE, TR or for staying within slew-rate limits
     rep = seq.test_report()

packages/se_t1_mapping.py
@@ -92,9 +92,13 @@ def constructor(
     # Inversion Pulse
     rf180inv, gz180inv, _ = pp.make_adiabatic_pulse(
         pulse_type = 'hypsec',
-        duration = 8e-3, beta = 800,
-        slice_thickness = slice_thickness, mu = 4.9,
-        return_gz = True, system = seq.system, use = "inversion"
+        duration = 8e-3, 
+        beta = 800,
+        slice_thickness = slice_thickness, 
+        mu = 4.9,
+        return_gz = True, 
+        system = seq.system, 
+        use = "inversion"
         )
 
 
@@ -106,7 +110,7 @@ def constructor(
             ##########################################################################################
             # SE module
             seq, TR, _,_,_ = SE_module(seq, fov = fov, Nx = Nx, Nz = Nz, Ny = Ny, TE = TE,
-                                TR = TR, ky_i = 0, ETL = etl, adc_duration = adc_duration, system = system,
+                                TR = TR, ky_i = ky_i, ETL = etl, adc_duration = adc_duration, system = system,
                                 channels = channels)
             ##########################################################################################
 
@@ -174,6 +178,7 @@ def SE_module(seq, fov = 200e-3, Nx = 128, Nz = 1, Ny = 128, TE = 15e-3,
     delta_kx = 1 / fov["ro"]
     delta_ky = 1 / fov["pe1"]
     kx_width = (Nx) * delta_kx
+
     gx = pp.make_trapezoid(channel = channels.ro, system = system, flat_area = kx_width,
                     flat_time = readout_time)
     adc = pp.make_adc(num_samples = Nx, duration = readout_time, delay = gx.rise_time)
@@ -191,7 +196,8 @@ def SE_module(seq, fov = 200e-3, Nx = 128, Nz = 1, Ny = 128, TE = 15e-3,
 
     flip90 = round(rf_flip * pi / 180, 3)
     flip180 = 180 * pi / 180
-    rf90, gz90, gz_reph = pp.make_sinc_pulse(flip_angle = flip90, system = system, 
+    rf90, gz90, gz_reph = pp.make_sinc_pulse(flip_angle = flip90, 
+                                             system = system, 
                                              duration = 2.5e-3,
                                              slice_thickness = slice_thickness, 
                                              apodization = 0.5,
@@ -200,39 +206,59 @@ def SE_module(seq, fov = 200e-3, Nx = 128, Nz = 1, Ny = 128, TE = 15e-3,
                                              return_gz = True, 
                                              use = "excitation")
 
-    rf180, gz180, _ = pp.make_sinc_pulse(flip_angle = flip180, system = system,
-                                      duration = 2.5e-3,
-                                      slice_thickness = 1.25 * slice_thickness,
-                                      apodization = 0.5,
-                                      time_bw_product = 4, 
-                                      phase_offset = 0,
-                                      return_gz = True, 
-                                      use = "refocusing")
+    rf180, gz180, _ = pp.make_sinc_pulse(flip_angle = flip180, 
+                                         system = system,
+                                         duration = 2.5e-3,
+                                         slice_thickness = 1.25 * slice_thickness,
+                                         apodization = 0.5,
+                                         time_bw_product = 4, 
+                                         phase_offset = 0,
+                                         return_gz = True, 
+                                         use = "refocusing")
 
     # Spoiler
     gss_spoil_A = 4 / slice_thickness
     gss_spoil = pp.make_trapezoid(channel = channels.pe2, system = system, area = gss_spoil_A)
 
-    gss_times = np.cumsum([0, gss_spoil.rise_time, gss_spoil.flat_time,
-                           gss_spoil.fall_time - gz180.rise_time, gz180.flat_time,
+    gss_times = np.cumsum([0, 
+                           gss_spoil.rise_time, 
+                           gss_spoil.flat_time,
+                           gss_spoil.fall_time - gz180.rise_time, 
+                           gz180.flat_time,
                            gss_spoil.rise_time - gz180.fall_time,
-                           gss_spoil.flat_time, gss_spoil.fall_time])
+                           gss_spoil.flat_time, 
+                           gss_spoil.fall_time])
 
-    gss_amps = np.array(
-            [0, gss_spoil.amplitude, gss_spoil.amplitude, gz180.amplitude, gz180.amplitude, gss_spoil.amplitude,
-             gss_spoil.amplitude, 0])
+    gss_amps = np.array([0, 
+                         gss_spoil.amplitude, 
+                         gss_spoil.amplitude, 
+                         gz180.amplitude, 
+                         gz180.amplitude, 
+                         gss_spoil.amplitude, 
+                         gss_spoil.amplitude, 
+                         0])
 
-    gss_spoil_add = pp.make_extended_trapezoid(channel = channels.pe2, amplitudes=gss_amps, times=gss_times, system=system)
-
-    rf180.delay = gss_spoil.rise_time + gss_spoil.flat_time + gss_spoil.fall_time - gz180.rise_time
-
+    gss_spoil_add = pp.make_extended_trapezoid(channel = channels.pe2, amplitudes = gss_amps, times = gss_times, system = system, convert_to_arbitrary=True)
     gss_spoil_duration = pp.calc_duration(gss_spoil_add)
 
     # End of TR spoiler
     gz_spoil = pp.make_trapezoid(channel = channels.pe2, system=system, area=2 / slice_thickness)
     gz_spoil.id = seq.register_grad_event(gz_spoil)
 
-    # Delays
+    # Spoiler on all axes
+    gz_s = pp.make_trapezoid(channel = channels.pe2, system = seq.system, area = -4 / slice_thickness)
+    gx_s = pp.make_trapezoid(channel = channels.ro, system = seq.system, area = -4 / slice_thickness)
+    gy_s = pp.make_trapezoid(channel = channels.pe1, system = seq.system, area = -4 / slice_thickness)
+
+    # Phase encoding
+    gy_pre = pp.make_trapezoid(channel = channels.pe1, system = system,
+                                 area = phase_areas[-ky_i - 1], duration = pe_duration)
+    gy_post = pp.make_trapezoid(channel = channels.pe1, system = system,
+                                  area = -phase_areas[-ky_i - 1], duration = pe_duration)
+
+    # Calculate timing for delays
+    rf180.delay = gss_spoil.rise_time + gss_spoil.flat_time + gss_spoil.fall_time - gz180.rise_time
+
     # Echo time (TE) and repetition time (TR)
     pre180d = (TE[0] / 2
                - pp.calc_duration(gz90) / 2
@@ -244,25 +270,13 @@ def SE_module(seq, fov = 200e-3, Nx = 128, Nz = 1, Ny = 128, TE = 15e-3,
                 - pe_duration
                 - (gx.rise_time + readout_time / 2)
                 )
-
     pre180delay = pp.make_delay(pre180d)
     post180delay= pp.make_delay(post180d)
 
-    # Spoiler on all axes
-    gz_s = pp.make_trapezoid(channel = channels.pe2, system = seq.system, area = -4 / slice_thickness)
-    gx_s = pp.make_trapezoid(channel = channels.ro, system = seq.system, area = -4 / slice_thickness)
-    gy_s = pp.make_trapezoid(channel = channels.pe1, system = seq.system, area = -4 / slice_thickness)
-
-
+    # TI delay and TR delay
     time_remove_TI = pp.calc_duration(gz90) + pp.calc_duration(gz_reph) + pp.calc_duration(gz180) \
           + pp.calc_duration(gss_spoil_add) - gz180.rise_time - pp.calc_duration(gx_s, gy_s, gz_s)
     remove_delayTR = time_remove_TI + pp.calc_duration(gx_post) + readout_time + pre180d
-
-
-    gy_pre = pp.make_trapezoid(channel = channels.pe1, system = system,
-                                 area = phase_areas[-ky_i - 1], duration = pe_duration)
-    gy_post = pp.make_trapezoid(channel = channels.pe1, system = system,
-                                  area = -phase_areas[-ky_i - 1], duration = pe_duration)
 
     # Build sequence block SE
     seq.add_block(gz_s, gx_s, gy_s)
@@ -272,7 +286,6 @@ def SE_module(seq, fov = 200e-3, Nx = 128, Nz = 1, Ny = 128, TE = 15e-3,
     seq.add_block(pre180delay)
     seq.add_block(rf180, gss_spoil_add)
     seq.add_block(post180delay)
-
 
     seq.add_block(gy_pre)
 

test.py
@@ -1,11 +1,41 @@
 import pypulseq as pp
+import numpy as np
+system = pp.Opts()
 
-from packages.mr_systems import lumina as system
+# Set max slew to 40 T/m/s
+system.max_slew = 40 * system.gamma  # Hz/m/s
 
-seq = pp.Sequence(system=system)
-seq.read(r'E:\Python\tse_3d_lumina_64_64_64.seq', detect_rf_use = True)
+gs_rise_time = 500e-6
+gs_fall_time = 500e-6
+gs_flat_time = 440e-6
+gs_amplitude = 851063.829787234
 
+grf_rise_time = 200e-6
+grf_fall_time = 200e-6
+grf_flat_time = 2.5e-3
+grf_amplitude = 256000
+gss_times = np.cumsum([0, 
+                        gs_rise_time, 
+                        gs_flat_time,
+                        gs_fall_time - grf_rise_time, 
+                        grf_flat_time,
+                        gs_rise_time - grf_fall_time,
+                        gs_flat_time, 
+                        gs_fall_time])
 
+gss_amps = np.array([0, 
+                        gs_amplitude, 
+                        gs_amplitude, 
+                        grf_amplitude, 
+                        grf_amplitude, 
+                        gs_amplitude, 
+                        gs_amplitude, 
+                        0])
 
+gss_spoil_add = pp.make_extended_trapezoid(channel = 'z', amplitudes = gss_amps, times = gss_times, system = system)
 
-print(seq.definitions)
+g = pp.points_to_waveform(times=gss_times, amplitudes=gss_amps, grad_raster_time=system.grad_raster_time)
+
+slew = np.squeeze(np.subtract(g[1:], g[:-1]) / system.grad_raster_time)
+print(f"Max slew rate of gradient: {round(max(slew)/system.gamma, 2)} T/m/s")
+